4a08d8516bd65cae3f0f47b2bbefffc1c617697d
Ximena Vila Ferral <hola@ximenavf.com>
2020-01-09 00:43:28 -0600

    add GitHub link, adjust styles, add README

---
+++ 1-project-one/README.md
@@ -0,0 +1,3 @@
+# Project 1: Toggle Password Visibility
+
+![](project1-screenshot1.jpg)
--- 1-project-one/scripts.js
+++ 1-project-one/scripts.js
@@ -1,12 +1,15 @@
 // Toggle Password Scripts //
 var checkPass = document.querySelector('#show-password');
-console.log(checkPass);
+var passwordInput = document.querySelector('#password');
 
 if (checkPass) {
     checkPass.addEventListener('click', function (showPassword) {
-        console.log(showPassword); // The event details
-        var passwordInput = document.querySelector('#password');
-        passwordInput.type = this.checked ? 'text' : 'password';
-        console.log(passwordInput.value);
+        if (checkPass.checked) {
+            password.type = 'text';
+        } else {
+            password.type = 'password';
+        }
     }, false);
 }
+
+
