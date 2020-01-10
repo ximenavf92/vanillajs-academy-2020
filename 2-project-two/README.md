8d1df0bb0a1f4d8ab28345c5c4ef143e6a4c72b8
Ximena Vila Ferral <hola@ximenavf.com>
2020-01-09 23:50:47 -0600

    initial commit for project 2

---
+++ 2-project-two/README.md
@@ -0,0 +1,3 @@
+# Project 2: Password Visibility - Multiple Fields
+
+![](project2-screenshot1.jpg)
---
+++ 2-project-two/scripts.js
@@ -0,0 +1,15 @@
+// Project 2 Scripts //
+// var checkPass = document.querySelectorALL('[type="password"]');
+// var passwordInput = document.querySelector('#password');
+
+// if (checkPass) {
+//     checkPass.addEventListener('click', function (showPassword) {
+//         if (checkPass.checked) {
+//             password.type = 'text';
+//         } else {
+//             password.type = 'password';
+//         }
+//     }, false);
+// }
+
+
