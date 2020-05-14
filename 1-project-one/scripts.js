cb66427f42a53e9801ade5946f3dcf25587db520
Ximena Vila Ferral <hola@ximenavf.com>
2020-05-13 19:26:37 -0500

    delete old js and write from scratch for May course solutions

--- 1-project-one/scripts.js
+++ 1-project-one/scripts.js
@@ -1,15 +1,12 @@
-// Toggle Password Scripts //
-var checkPass = document.querySelector('#show-password');
-var passwordInput = document.querySelector('#password');
+var getPassword = document.querySelector('#password'); // Get input with id of password
+var checkbox = document.querySelector('#show-password'); // Get checkbox with id of show-password
 
-if (checkPass) {
-    checkPass.addEventListener('click', function (showPassword) {
-        if (checkPass.checked) {
-            password.type = 'text';
+if (checkbox) { // if element stored in checkbox exists trigger the following event
+    checkbox.addEventListener('click', function (event) { // when checkbox is clicked
+        if (checkbox.checked) {
+            getPassword.type = "text"; // If checked change input type to text and reveal password input value
         } else {
-            password.type = 'password';
+            getPassword.type = "password"; // if not checked set type to password and conceal password input value
         }
-    }, false);
+    })
 }
-
-
